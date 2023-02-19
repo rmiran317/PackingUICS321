54c541875fd8c9ef2c002420e5d8a263005336ff
ronthedon <ronaldm7777@gmail.com>
2023-02-19 16:02:45 -0500

    Added page for putPackage button

---
+++ app/src/main/java/com/example/packingui/GetPackPage.java
@@ -0,0 +1,14 @@
+package com.example.packingui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GetPackPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_pack_page);
+    }
+}
