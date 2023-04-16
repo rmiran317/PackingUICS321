986e5e96276e40723dbdbb748d899d117cf4574f
ronthedon <ronaldm7777@gmail.com>
2023-04-16 16:10:39 -0400

    New search page: added title, search bar, and button Later to be used as an input for a package Started modifying GetPackPage.java to implement a variable that will be used with the input for Search entry

--- app/src/main/java/com/example/packingui/GetPackPage.java
+++ app/src/main/java/com/example/packingui/GetPackPage.java
@@ -3,12 +3,16 @@ package com.example.packingui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GetPackPage extends AppCompatActivity {
 
+    TextView searchPackNum;
+    public static final int SearchPackNum = 00000000000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_pack_page);
+        searchPackNum = (TextView) findViewById(R.id.search_package_entry);
     }
 }
